9cfba5c8a9afc202045c6356c6565b1094be0468
angelLuisMadrid <madrid.angel.luis@gmail.com>
2020-06-25 10:43:16 +0200

    Modificación de lógica de acceso a datos
    Eliminación de invocaciones a SQL mediante PHP y eliminación de beans y DAO.

--- app/src/main/java/com/personajes/Quiz.java
+++ app/src/main/java/com/personajes/Quiz.java
@@ -1,18 +1,12 @@
 package com.personajes;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.example.quizledt.R;
 
-import java.util.HashMap;
 
-import com.personajes.bean.QuizBean;
-import com.personajes.dao.QuizDao;
-import com.personajes.util.HttpHandler;
 
 /**
  * Created by Engueru on 02/06/2018.
@@ -20,54 +14,13 @@ import com.personajes.util.HttpHandler;
 
 public class Quiz extends Activity {
 
-    //
     private TextView tvpregunta;
-    private final String URL_WEBSERVICE = "http://www.laeradeltitan.com/getQuestion.php";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_layout);
-        cargarPreguntas("1","cargarPregunta");
 
         tvpregunta = (TextView) findViewById(R.id.TvQuizPegunta);
 
     }
 
-    public void cargarPreguntas(String param, String tag){
-        Log.i("error", "cargarPreguntas");
-        HashMap<String, String> params= new HashMap<String, String>();
-        params.put("url",URL_WEBSERVICE);
-        params.put("tag",tag);
-        params.put("id", param);
-        Quiz.AsyncHttpTask task = new Quiz.AsyncHttpTask();
-        task.execute(params);
-    }
-
-    public class AsyncHttpTask extends AsyncTask<HashMap<String,String>, Void, String> {
-
-        //DURANTE EL PROCESO...
-        @Override
-        protected String doInBackground(HashMap<String, String>... params) {
-            Log.i("error", "AsyncHttpTask");
-            //EJECUTAMOS LA CLASE HANDLER QUE LLAMA AL SERVIDOR JUNTO CON LOS PARAMETROS QUE HEMOS INTRODUCIDO
-            HttpHandler handler = new HttpHandler();
-            String url = params[0].get("url");
-            params[0].remove("url");
-            String response = handler.mainPerformPostCall(url, params[0]);
-            Log.i("error", "Aqui la respuesta al asynctask : "+response);
-            return response;
-        }
-
-        //UNA VEZ FINALIZADO EL PROCESO Y CARGADO EL STRING DE RESULTADO...
-        @Override
-        protected void onPostExecute(String result) {
-            //SI OBTENEMOS RESULTADO..
-            if (result.length() > 0) {
-                QuizDao qDAO = new QuizDao();
-                QuizBean bean = qDAO.getQuestion(result);
-                tvpregunta.setText(bean.getDescription_answer()+"");
-            } else {
-                Log.i("error", "no cargaron los datos!");
-            }
-        }
-    }
 }
